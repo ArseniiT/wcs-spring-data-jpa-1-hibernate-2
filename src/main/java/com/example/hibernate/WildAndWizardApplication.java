10017fbaaf4678a553f37114e9fb2e2015ce88a4
arseniit <arseniit@gmail.com>
2024-01-12 15:32:04 +0100

    Initial commit as it is on WCS page of Spring Data JPA 1 : Hibernate

--- src/main/java/com/example/hibernate/WildAndWizardApplication.java
+++ src/main/java/com/example/hibernate/WildAndWizardApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernateApplication {
+public class WildAndWizardApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(HibernateApplication.class, args);
+		SpringApplication.run(WildAndWizardApplication.class, args);
 	}
 
 }
