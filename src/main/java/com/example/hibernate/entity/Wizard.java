9315063914b3c0b4e525c164051411bfb8aa0504
arseniit <arseniit@gmail.com>
2024-01-18 00:02:46 +0100

    Fixed convert value of type java.lang.String to required type java.util.Date

--- src/main/java/com/example/hibernate/entity/Wizard.java
+++ src/main/java/com/example/hibernate/entity/Wizard.java
@@ -1,6 +1,7 @@
 package com.example.hibernate.entity;
 
 import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -12,6 +13,7 @@ public class Wizard {
     private Long id;
     private String firstName;
     private String lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
     private String birthPlace;
     private String biography;
